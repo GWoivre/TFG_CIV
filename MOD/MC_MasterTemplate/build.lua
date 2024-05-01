3ca1d3051d14c52d177b271ef88c3e7564aedc50
GWoivre <guillermowoivre@gmail.com>
2024-05-01 17:43:31 +0200

    Creada accion LUA

--- MOD/MC_MasterTemplate/build.lua
+++ MOD/MC_MasterTemplate/build.lua
@@ -1,4 +1,17 @@
--- LuaScript1
--- Author: abedu
--- DateCreated: 4/26/2024 6:37:51 PM
---------------------------------------------------------------
+local iBuilding = GameInfoTypes.BUILDING_CASINO
+local RequiredCivilization = GameInfoTypes.CIVILIZATION_GWG_VEGAS
+
+function CasinoIncome(iPlayer)
+	local pPlayer = Players[iPlayer] -- sacamos al jugador
+	if pPlayer:GetCivilizationType() ~= RequiredCivilization then return end -- si no es la civilizacion de las Vegas termina
+	if not pPlayer:IsAlive() then return end -- si esta muerto termina
+	for pCity in pPlayer:Cities() do -- por cada ciudad que tenemos
+		if pCity:HasBuilding(iBuilding) then -- ver si tenemos un casino en esa ciudad
+			local val = pPlayer:GetTreasury():GetGoldBalance() -- Cogemos el valor original
+			local rand = math.random(0,5) -- cogemos un valor aleatorio entre 0 y 5
+			Player:GetTreasury():SetGoldBalance(val + rand) -- lo añadimos al balance original
+		end
+	end
+end
+GameEvents.PlayerTurnStarted.Add(CasinoIncome) -- Y por ultimo lo añadimos al GameEvents de PlayerTurnStarted queriendo decir que esto se activa al inicio de cada turno del jugador
+
